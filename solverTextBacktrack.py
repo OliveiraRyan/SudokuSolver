edcc179a107fe34ce35c2ff5a48ff3046aca6390
Ryan Oliveira <oliveira.ryan@outlook.com>
2021-04-16 02:21:33 -0400

    Fixed the issues with multiple solutions. Added QoL and optimized where I could.

--- solverTextBacktrack.py
+++ solverTextBacktrack.py
@@ -13,79 +13,103 @@ board = [
 ]
 
 _MAX_VALUE = 9
+_BOARD_SIZE = 9
 
-def solve(bo, xpos, ypos):
-    if (xpos == None or ypos == None):
-        return True
-    
-    for i in range(1, _MAX_VALUE+1):
-        if (is_valid(bo, i, (xpos, ypos))):
-            bo[xpos][ypos] = i
-        
-            if (solve(bo, *find_empty(board, xpos, ypos+1))):
-                return True
+num_solutions = 0
+keep_going = False
 
-            bo[xpos][ypos] = 0
+def solve(r = 0, c = 0):
+    global board
+    global num_solutions
+    global keep_going
 
-    return False
+    for col in range(c, _MAX_VALUE):
+        # updates row 
+        if (col == c):
+            ro = r
+        else:
+            ro = 0
+        for row in range(ro, _MAX_VALUE):
+            if (board[row][col] == 0):
+                for n in range(1,10):
+                    if (is_valid(n, (row, col))):
+                        board[row][col] = n
+                        solve(row, col)
+                        board[row][col] = 0
+                return
 
-def is_valid(bo, num, pos):
-    # Check row
-    for i in range(len(bo[0])):
-        if bo[pos[0]][i] == num and pos[1] != i:
-            return False
+    num_solutions += 1
+    if (num_solutions == 1):
+        print("\nSolved board:")
+        print_board()
+    elif (num_solutions == 2):
+        answer = ''
+        while (answer.lower() != 'y' and answer.lower() != 'n'):
+            answer = input("This board does not have a unique solution. Keep going and count the number of solutions? [Y/n] ")
 
-    # Check column
-    for i in range(len(bo)):
-        if bo[i][pos[1]] == num and pos[0] != i:
-            return False
+        if (answer.lower() == 'y'):
+            keep_going = True
+        else:
+            exit(0)
+            
+    return
+                
 
-    # Check box
+
+def is_valid(num, pos):
+    """
+    Returns True if the attempted move is valid, False otherwise
+    :param pos: (row, col)
+    :param num: int
+    :return: bool
+    """
+
+    # get box position
     box_x = pos[1] // 3
     box_y = pos[0] // 3
 
+    # Check row
+    for i in range(_BOARD_SIZE):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(_BOARD_SIZE):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
     for i in range(box_y*3, box_y*3 + 3):
         for j in range(box_x * 3, box_x*3 + 3):
-            if bo[i][j] == num and (i,j) != pos:
+            if board[i][j] == num and (i,j) != pos:
                 return False
 
     return True
 
-
-def find_empty(bo, row, col):
-    while(row < 9):
-        while(col < 9):
-            if (bo[row][col] == 0):
-                return (row, col)
-            col += 1
-        col = 0
-        row += 1
-    
-    return (None, None)
-
-def print_board(bo):
+def print_board():
     '''
     Prints the board, formatted to look like a sudoku grid
     '''
 
     print(" ───────────────────────")
-    for i in range(len(bo)):
+    for i in range(len(board)):
         if i % 3 == 0 and i != 0:
             print(" ───────────────────────")
-        for j in range(len(bo[0])):
+        for j in range(len(board[0])):
             if j % 3 == 0:
                 print("| ",end="")
 
             if j == 8:
-                print(str(bo[i][j]) + " |")
+                print(str(board[i][j]) + " |")
             else:
-                print(str(bo[i][j]) + " ", end="")    
+                print(str(board[i][j]) + " ", end="")    
     print(" ───────────────────────")
 
+
 print("Unsolved board:")
-print_board(board)
+print_board()
 start = time.time()
-solve(board, *find_empty(board, 0, 0))
-print("\nSolved board:")
-print_board(board)
-print ("My program took", time.time() - start, "to run")
+solve()
+if(num_solutions != 1):
+    print("The number of solutions: {}".format(num_solutions))
+print ("Checking all possibilities took", time.time() - start, "seconds")
